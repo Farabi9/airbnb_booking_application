defcd90bd03bf5a56d90c5c1fe97ce3a144144cd
Farabi Rifat <ifatkhan633@gmail.com>
2023-06-01 19:57:51 +0600

    form upload file attached with api for making it work on server

--- src/Pages/PlacesPage.jsx
+++ src/Pages/PlacesPage.jsx
@@ -50,6 +50,23 @@ export default function PlacesPage() {
         setPhotoLink('')
     }
 
+     function uploadPhoto(ev){
+        const files= ev.target.files;
+        const data = new FormData;
+       for(let i =0; i< files.length; i++){
+        data.append('photos', files[i])
+       }
+        
+        axios.post('/upload', data, {
+            headers: {'Content-type': 'multipart/form-data'}
+        }).then(response =>{
+            const {data: filename} = response;
+            setAddPhotos( prev => {
+                return[...prev, filename]
+            })
+        })
+        }
+
   return (
         <div>
             {action !== 'new' &&(
@@ -78,6 +95,7 @@ export default function PlacesPage() {
                         <input type="text " value={photoLink} onChange={ev => setPhotoLink(ev.target.value)} placeholder={'Add using a link ....jpg'} />
                         <button onClick={addPhotoByLink} className='bg-gray-200 py-4 rounded-2xl gap-3 px-4 '>Add&nbsp;photo</button>
                     </div>
+                    
                    <div className='mt-2 gap-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
 
 
@@ -88,12 +106,13 @@ export default function PlacesPage() {
                     </div>
                 ))}
 
-                   <button className='flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600'>
+                   <label className='cursor-pointer flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600'>
+                   <input type="file" multiple className='hidden' onChange={uploadPhoto} />
                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
 </svg>
 
-                    Upload</button>
+                    Upload</label>
                    </div>
                    {preInput('Description', 'descriotion of the page')}
                    
